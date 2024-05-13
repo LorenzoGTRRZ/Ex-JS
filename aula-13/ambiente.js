be84535533c2fa732c9cddac8a76dfa8bfbe6076
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-05-13 16:56:20 -0300

    Add aula 13 e 14

---
+++ aula-13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++ //  c = c + 1
+} while (c <= 6)
+
+/* var c = 1
+while (c <= 50) {
+    console.log(`passo ${c}`)
+    c++ //  c = c + 1
+} */
+
+/* console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?') */
